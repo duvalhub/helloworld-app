9466e52102e68bb89c213933c54a987885080604
Maxthod <47720660+Maxthod@users.noreply.github.com>
2021-04-06 23:46:32 -0400

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React (test pour voir) 
+          Learn React (test pour voir)  WAZZUP BABE
         </a>
       </header>
     </div>
